2df7483e2e42b2c9670b63d3b7a43027551486a6
Simon Cropp <simon.cropp@gmail.com>
2014-09-17 15:48:35 +1000

    add unit tests

---
+++ src/NServiceBus.CastleWindsor.Tests/SetUpFixture.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus.ContainerTests;
+using NServiceBus.ObjectBuilder.CastleWindsor;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class SetUpFixture
+{
+    [SetUp]
+    public void Setup()
+    {
+        TestContainerBuilder.ConstructBuilder = () => new WindsorObjectBuilder();
+    }
+
+}
