01cf384fdf069e5ea740cd49124f2ef20f52036b
Andreas Öhlund <andreas.ohlund@particular.net>
2016-08-31 15:31:12 +0200

    Update to rc1 (#16)
    * Update packages to beta 7
    
    * Updating to core 6.0.0-RC1
    
    * Require core rc1 in nuspec

---
+++ src/NServiceBus.CastleWindsor.AcceptanceTests/When_using_TypedFactoryFacility.cs
@@ -0,0 +1,59 @@
+﻿namespace NServiceBus.AcceptanceTests.CastleWindsor
+{
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+    using Castle.Windsor;
+    using Castle.Facilities.TypedFactory;
+    using System;
+    using Newtonsoft.Json;
+    public class When_using_TypedFactoryFacility : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task should_not_error()
+        {
+            var res = await Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(b => b.When(bus => bus.SendLocal(new MyMessage())))
+                .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                var container = new WindsorContainer();
+                container.AddFacility<TypedFactoryFacility>();
+
+                EndpointSetup<DefaultServer>(config =>
+                {
+                    config.ExcludeAssemblies(typeof(NewtonsoftSerializer).Assembly.FullName);
+                    config.UseSerialization<NewtonsoftSerializer>();
+                    config.UseContainer<WindsorBuilder>(b => b.ExistingContainer(container));
+                });
+            }
+
+            public class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                public async Task Handle(MyMessage message, IMessageHandlerContext context)
+                {
+                    await context.SendLocal(new Reply { });
+                }
+            }
+
+        }
+
+        public class Reply : ICommand
+        {
+        }
+
+        public class MyMessage : ICommand
+        {
+        }
+    }
+}
