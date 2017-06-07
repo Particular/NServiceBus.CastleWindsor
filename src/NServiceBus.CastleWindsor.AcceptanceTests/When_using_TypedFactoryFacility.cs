993c98f352b76e23b23cb8d2c807419485c10774
Simon Cropp <simon.cropp@gmail.com>
2017-06-07 13:12:46 +1000

    move AcceptanceTests to sdk project

--- src/NServiceBus.CastleWindsor.AcceptanceTests/When_using_TypedFactoryFacility.cs
+++ src/NServiceBus.CastleWindsor.AcceptanceTests/When_using_TypedFactoryFacility.cs
@@ -1,20 +1,19 @@
 ﻿namespace NServiceBus.AcceptanceTests.CastleWindsor
 {
     using System.Threading.Tasks;
-    using NServiceBus.AcceptanceTesting;
-    using NServiceBus.AcceptanceTests;
-    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using AcceptanceTesting;
+    using AcceptanceTests;
+    using EndpointTemplates;
     using NUnit.Framework;
     using Castle.Windsor;
     using Castle.Facilities.TypedFactory;
-    using System;
-    using Newtonsoft.Json;
+
     public class When_using_TypedFactoryFacility : NServiceBusAcceptanceTest
     {
         [Test]
-        public async Task should_not_error()
+        public Task should_not_error()
         {
-            var res = await Scenario.Define<Context>()
+            return Scenario.Define<Context>()
                 .WithEndpoint<Endpoint>(b => b.When(bus => bus.SendLocal(new MyMessage())))
                 .Run();
         }
@@ -32,8 +31,7 @@
 
                 EndpointSetup<DefaultServer>(config =>
                 {
-                    config.ExcludeAssemblies(typeof(NewtonsoftSerializer).Assembly.FullName);
-                    config.UseSerialization<NewtonsoftSerializer>();
+                    config.SendFailedMessagesTo("error");
                     config.UseContainer<WindsorBuilder>(b => b.ExistingContainer(container));
                 });
             }
@@ -42,7 +40,7 @@
             {
                 public async Task Handle(MyMessage message, IMessageHandlerContext context)
                 {
-                    await context.SendLocal(new Reply { });
+                    await context.SendLocal(new Reply());
                 }
             }
 
