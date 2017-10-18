6513959653ed23c868df5af53b9e6d7b71a6097a
Bob Langley <bob.langley@particular.net>
2017-10-18 15:57:47 -0700

    Add diagnostics feature to report if using existing container

---
+++ src/NServiceBus.CastleWindsor/WindsorDiagnostics.cs
@@ -0,0 +1,28 @@
+﻿namespace NServiceBus.Features
+{
+    /// <summary>
+    /// Adds diagnostics information for NServiceBus.CastleWindsor
+    /// </summary>
+    public class WindsorDiagnostics : Feature
+    {
+        /// <summary>
+        /// Constructor for NServiceBus.CastleWindsor diagnostics feature
+        /// </summary>
+        public WindsorDiagnostics()
+        {
+            EnableByDefault();
+        }
+
+        /// <summary>
+        /// Sets up CastleWindsor diagnostics
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Settings.AddStartupDiagnosticsSection("NServiceBus.CastleWindsor", new
+            {
+                UsingExistingContainer = context.Settings.HasSetting<WindsorBuilder.ContainerHolder>()
+            });
+        }
+    }
+}
